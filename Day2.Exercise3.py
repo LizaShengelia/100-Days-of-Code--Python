1c02ed1b0bae9ffaa1b1633566802560bc5cbbcd
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2021-09-17 19:03:46 +0400

    Create Day2.Exercise3.py

---
+++ Day2.Exercise3.py
@@ -0,0 +1,10 @@
+age = input("What is your current age?")
+
+new_age = 90 - int(age)
+
+x = int(new_age) * 365
+y = int(new_age) * 52
+z = int(new_age) * 12
+
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
